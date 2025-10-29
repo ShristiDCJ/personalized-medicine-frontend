b920c3d3fe95a18fa3adbcbd55da252eb079705c
Shristi Mishra <shristi.college.mishra@gmail.com>
2025-10-29 20:28:59 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 // app/layout.js
 import './globals.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
